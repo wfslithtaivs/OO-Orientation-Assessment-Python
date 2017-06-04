c57c26efe9cb2d97f199c997b77d8576632b6671
vagrant <vagrant@vagrant.vm>
2017-06-04 23:20:59 +0000

    Initial commit

---
+++ assessment.py
@@ -0,0 +1,53 @@
+"""
+Part 1: Discussion
+
+1. What are the three main design advantages that object orientation
+   can provide? Explain each concept.
+
+   Object-orientation require next 3 things:
+
+    Abstraction - allows to abstract certain logic.
+
+    Incapsulation - allows to abstract complicated object-behaviour.
+
+    Polymorphism - allows using "generally alike" objects similar ways
+    without any knowledge of implementation details.
+
+2. What is a class?
+
+    Class in Python is a specific data type that subclasses (by default) Class Object
+    and abstract certain behaviour pattern.
+    Designed to implement object-oriented concepts.
+
+3. What is an instance attribute?
+
+    Any attribute that might be accessed using following syntax:
+        class_instance.attribute_name
+
+    Depends on context might return instance specific value
+    or value for higher level class (up to Object) one.
+
+4. What is a method?
+
+    Method is a function belongs to a specific class.
+
+5. What is an instance in object orientation?
+
+    Instance in OO is an object of a specific class.
+
+6. How is a class attribute different than an instance attribute?
+   Give an example of when you might use each.
+
+   Class attribute belongs to class and might be accessed by instances of class.
+   Instance attribute belongs to instance of class and can't accessed from class level.
+
+   Usage:
+    class attribute count keeps track of number of class instances ever created.
+
+    instance attribute name stores specific name of cat of class Cat.
+"""
+
+
+# Parts 2 through 5:
+# Create your classes and class methods
+
