c22bb91e710b252ea41ea30700415dbbb023aafe
vagrant <vagrant@vagrant.vm>
2017-06-05 00:20:18 +0000

    Complete parts 1-5

--- assessment.py
+++ assessment.py
@@ -47,7 +47,118 @@ Part 1: Discussion
     instance attribute name stores specific name of cat of class Cat.
 """
 
-
 # Parts 2 through 5:
 # Create your classes and class methods
 
+class Student(object):
+    """Class Student"""
+
+    def __init__(self, fname, lname, address):
+        """ Initialises instance attributes"""
+
+        self.first_name = fname
+        self.last_name = lname
+        self.address = address
+
+
+class Question(object):
+    """ Class Question"""
+
+    def __init__(self, question, answer):
+        """Initialises instance attributes"""
+
+        self.question = question
+        self.correct_answer = answer
+
+
+class Exam(object):
+    """ Class Exam"""
+
+    def __init__(self, name):
+        """Initialises instance attributes"""
+
+        self.name = name
+        self.questions = []
+
+    def add_question(self, question):
+        """Adds question object to the list of exam questions"""
+
+        self.questions.append(question)
+
+    def administer(self):
+        """Run exam"""
+
+        num_right_answers = 0
+        total_questions = len(self.questions)
+
+        for i in range(total_questions):
+            student_answer = raw_input(self.questions[i].question + " > ")
+            if student_answer == self.questions[i].correct_answer:
+                num_right_answers += 1
+
+        return (float(num_right_answers)/total_questions) * 100
+
+
+class Quiz(Exam):
+    """Class Quiz"""
+
+    def administer(self):
+        """Return pass or fail"""
+
+        res = super(Quiz, self).administer()
+        return int(res >= 50)
+
+
+class StudentExam(object):
+    """ Class StudentExam to allow Student to take an Exam and store result"""
+
+    def __init__(self, student, test):
+        """Initialises instance attributes"""
+
+        self.student = student
+        self.test = test
+
+    def take_test(self):
+        """Take exam and store result for student"""
+
+        self.student_score = self.test.administer()
+
+
+class StudentQuiz(StudentExam):
+    """ Class StudentQuiz to allow Student to take a Quiz and store result.
+        Same logic, just a different name """
+
+
+def example():
+    """ Creates objects for student and exam and allows student to take exam
+        using StudentExam object"""
+
+    # create object of exam class and add questions
+    exam = Exam('midterm')
+    print "Welcome to %s exam:" % (exam.name)
+    set_q = Question('What is the method for adding an element to a set?', '.add()')
+    exam.add_question(set_q)
+    pwd_q = Question('What does pwd stand for?', 'print working directory')
+    exam.add_question(pwd_q)
+    list_q = Question('Python lists are mutable, iterable, and what?', 'ordered')
+    exam.add_question(list_q)
+    # create object of Student class
+    student = Student("Kate", "S", "/usr/bin/python")
+    # create object of StudentExam class and pass student and exam as a parameters
+    student_exam = StudentExam(student, exam)
+    student_exam.take_test()
+    # print result of taking result by student
+    print "Your exam score is", student_exam.student_score
+
+    # Take weekly Quiz
+    quiz = Quiz('weekly')
+    print "Welcome to %s quiz: " % (quiz.name)
+    quiz.add_question(set_q)
+    quiz.add_question(pwd_q)
+    quiz.add_question(list_q)
+    student_quiz = StudentQuiz(student, quiz)
+    student_quiz.take_test()
+    print "Your quiz score is", student_quiz.student_score
+
+if __name__ == '__main__':
+    example()
